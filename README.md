5f81180ad30b0622da880f5277fcd2faf0d9c937
Erik Jonsson <erik.jonsson@hiq.se>
2026-03-13 12:05:11 +0100

    Adding main skeleton code and cmake dependecies stuff

--- README.md
+++ README.md
@@ -1,3 +1,21 @@
 COMP4300
-2025: https://www.youtube.com/watch?v=3WWjsTPdlag&list=PL_xRyXins848Mfiv4hIgiMZgLpzFT7ohv&index=21
+
+# Links
+**2025 fall lecture**
+https://www.youtube.com/watch?v=3WWjsTPdlag&list=PL_xRyXins848Mfiv4hIgiMZgLpzFT7ohv&index=21
+
+**How to get full hash of commit/release/tag**
+https://stackoverflow.com/questions/949314/how-do-i-get-the-hash-for-the-current-commit-in-git
+
+**imgui repo**
+https://github.com/ocornut/imgui
+
+**imgui-sfml repo**
+https://github.com/SFML/imgui-sfml?tab=readme-ov-file
+
+**fetch content (cmake) tutorial**
+https://github.com/eliasdaler/cmake-fetchcontent-tutorial-code
+
+**cmake fetch sfml, imgui, imgui-sfml**
+https://edw.is/using-cmake/
 
---
+++ src/main.cpp
@@ -0,0 +1,130 @@
+#include <filesystem>
+#include <iostream>
+#include <memory>
+#include <fstream>
+
+#include <SFML/Graphics.hpp>
+#include "imgui.h"
+#include "imgui-SFML.h"
+
+/*******************************
+TODOS
+- read from config file instead (assets/config.txt) 
+    - window
+    - shapes
+    - font 
+    - yadayada
+- have some container that stores shapes (the ones found in config file)
+- draw shapes
+********************************/
+int main(int argc, char* argv[])
+{
+    
+    const int wWidth = 1280;
+    const int wHeight = 720;
+
+    sf::RenderWindow window(sf::VideoMode({ wWidth, wHeight }), "SFML works!");
+    window.setFramerateLimit(60);
+
+    if(!ImGui::SFML::Init(window))
+    {
+        std::cout << "Could not init window" << "\n";
+        std::exit(-1);
+    }
+
+    sf::Clock deltaClock;
+
+    ImGui::GetStyle().ScaleAllSizes(2.0f);
+    ImGui::GetIO().FontGlobalScale = 2.0f;
+
+    // One per shape?
+    float c[3] = { 0.0f, 1.0f, 1.0f };
+
+    // A test shape, take from config file instead
+    float circleRadius = 50;
+    int circleSegments = 32;
+    float circleSpeedX = 1.0f;
+    float circleSpeedY = 0.5f;
+    bool drawCircle = true;
+    bool drawText = true;
+    
+    sf::CircleShape circle(circleRadius, circleSegments);
+    circle.setPosition({ 10.0f, 10.0f });
+
+    sf::Font font;
+
+    if(!font.openFromFile("assets/javatext.tff"))
+    {
+        std::cout << "Could not open font file!" << "\n";
+        std::exit(-1);
+    }
+    sf::Text text(font, "Sample Text", 24);
+
+    text.setPosition({ 0, wHeight - (float)(text.getCharacterSize())});
+
+    char displayString[255] = "Sample Text";
+    
+    // main loop
+    while(window.isOpen())
+    {
+        while(auto event = window.pollEvent())
+        {
+            ImGui::SFML::ProcessEvent(window, *event);
+            if(event->is<sf::Event::Closed>())
+            {
+                window.close();
+            }
+
+            if(const auto* keyPressed = event->getIf<sf::Event::KeyPressed>())
+            {
+                std::cout << "Key pressed with code = " << (int)keyPressed->scancode << "\n";
+                if(keyPressed->scancode == sf::Keyboard::Scancode::X)
+                {
+                    circleSpeedX *= -1.0f;
+                }
+
+            }   
+
+            ImGui::SFML::Update(window, deltaClock.restart());
+
+            ImGui::Begin("Window title!");
+            ImGui::Text("Window text");
+            ImGui::Checkbox("Draw Circle", &drawCircle);
+            ImGui::SameLine();
+            ImGui::Checkbox("Draw Text", &drawText);
+            ImGui::SliderFloat("Radius", &circleRadius, 0.0f, 300.0f);
+            ImGui::SliderInt("Sides", &circleSegments, 3, 64);
+            ImGui::ColorEdit3("Color Circle", c);
+            ImGui::InputText("Text", displayString, 255);
+            if(ImGui::Button("Set Text"))
+            {
+                text.setString(displayString);
+            }
+            ImGui::SameLane();
+            if(ImGui::Button("Reset Circle"))
+            {
+                 circle.setPosition({0, 0});
+            }
+            ImGui::End();
+
+            circle.setPointCount(circleSegments);
+            circle.setRadius(circleRadius);
+            circle.setFillColor(sf::Color(uint8_t(c[0]*255), uint8_t(c[1]*255), uint8_t(c[2]*255)));
+            circle.setPosition({circle.getPosition().x + circleSpeedX, circle.getPosition().y + circleSpeedY});
+
+            window.clear();
+            if(drawCircle)
+            {
+                window.draw(circle);
+            }
+            if(drawText)
+            {
+                window.draw(text);
+            }
+            ImGui::SFML::Render(window);
+            window.display();
+        }    
+
+        return 0;
+    }
+}
