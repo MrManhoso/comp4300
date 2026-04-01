bd732340e836c3ace0b418ca96cb46294af98723
Erik Jonsson <erik.jonsson@hiq.se>
2026-04-01 11:07:40 +0200

    continuing with vec2

--- inc/data/vec2.h
+++ inc/data/vec2.h
@@ -2,7 +2,7 @@
 #define VEC2_h
 
 #include "inc/concepts.h"
-
+#include <math.h>
 #include "SFML/Graphics.hpp"
 
 template <Numerical T>
@@ -18,19 +18,48 @@ class vec2
     vec2(const sf::Vector2i& vec) : x(vec.x), y(vec.y) {}
     
     operator sf::Vector2<T>() { return sf::Vector2<T>(x, y); }
-    bool operator==(const vec2& rhs) {}
-    bool operator!=(const vec2& rhs) {}
-    vec2 operator+(const vec2& rhs) {}
-    vec2 operator-(const vec2& rhs) {}
-    vec2 operator*(const vec2& rhs) {}
+    bool operator==(const vec2& rhs) { return x == rhs.x && y == rhs.y; }
+    bool operator!=(const vec2& rhs) { return !(*this == rhs); }
+    vec2 operator+(const vec2& rhs)
+    { 
+        x += rhs.x; 
+        y += rhs.y;
+        return *this; 
+    }
+    vec2 operator-(const vec2& rhs)
+    { 
+        x -= rhs.x; 
+        y -= rhs.y;
+        return *this; 
+    }
+    vec2 operator*(const vec2& rhs)
+    { 
+        x -= rhs.x; 
+        y -= rhs.y;
+        return *this; 
+    }
     vec2 operator/(const vec2& rhs) {}
     void operator+=(const vec2& rhs) {}
     void operator-=(const vec2& rhs) {}
     void operator*=(const vec2& rhs) {}
     void operator/=(const vec2& rhs) {}
     
-    void normalize() {}
-    float length() {}
+    // Speed is the length (magnitude) of the (velocity) vector
+    float length() const {}
+    float dist(const vec2& rhs) const 
+    { 
+        auto dx = rhs.x - x;  
+        auto dy = rhs.y - y;  
+        return std::sqrtf(dx*dx, dy*dy);
+    }
+
+    void normalize() 
+    {
+        auto l = length();
+        // TODO type conversion/rounding?
+        x = x/L;
+        y = y/L;
+    }
 };
 
 using vec2f = vec2<float>;
